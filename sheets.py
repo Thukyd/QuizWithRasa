feae53eabba5a96f6173d2c277e3c52883d19647
DESKTOP-JP-HOME\Tebbe <jptebbe@gmail.com>
2019-11-08 12:43:28 +0100

    Add Google Sheets Example file as database for quiz content

--- sheets.py
+++ sheets.py
@@ -6,7 +6,7 @@ from pprint import pprint
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
 # set up credentials
-creds = ServiceAccountCredentials.from_json_keyfile_name("./credentials/creds.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("./credentials/sheet_credentials.json", scope)
 
 client = gspread.authorize(creds)
 
@@ -14,13 +14,5 @@ sheet = client.open("Learning NLU").sheet1  # Open the spreadhseet
 
 data = sheet.get_all_records()  # Get a list of all records
 
-row = sheet.row_values(3)  # Get a specific row
-col = sheet.col_values(3)  # Get a specific column
-cell = sheet.cell(1,2).value  # Get the value of a specific cell
+pprint(data)
 
-insertRow = ["hello", 5, "red", "blue"]
-sheet.add_rows(insertRow, 66)  # Insert the list as a row at index 66
-
-sheet.update_cell(77,2, "CHANGED")  # Update one cell
-
-numRows = sheet.row_count  # Get the number of rows in the sheet
