09d019527239e6ecf3818f491e898e8695615921
Jan-Philipp Tebbe <jptebbe@hotmail.com>
2019-11-12 07:58:56 +0100

    Update sheets.py
    set totos inside sheets

--- sheets.py
+++ sheets.py
@@ -1,18 +1,16 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from pprint import pprint
 
+###### Configuration Sheets
 # set scope for access
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
 # set up credentials
 creds = ServiceAccountCredentials.from_json_keyfile_name("./credentials/sheet_credentials.json", scope)
-
 client = gspread.authorize(creds)
-
+# get sheets data
 sheet = client.open("Learning NLU").sheet1  # Open the spreadhseet
-
 data = sheet.get_all_records()  # Get a list of all records
 
+###### Operations
+# TODOs
 pprint(data)
-
