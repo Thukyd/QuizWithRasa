52d17924eaa3bbaece9e8f3e791fde0e0a018d2e
DESKTOP-JP-HOME\Tebbe <jptebbe@gmail.com>
2019-11-25 13:17:13 +0100

    Add FunFact in sheets logic

--- sheets.py
+++ sheets.py
@@ -17,61 +17,71 @@ data = sheet.get_all_records()  # Get a list of all records
 
 # mark true/false & shuffle
 def shuffleAnswers(colX, colY, colZ):
-    shuffleArray = [[colX, True], [colY, False] , [colZ, False]]
-    random.shuffle(shuffleArray)
-    data = {
-        "a" : shuffleArray[0],
-        "b" : shuffleArray[1],
-        "c" : shuffleArray[2],
-    }
-    return data
+	shuffleArray = [[colX, True], [colY, False] , [colZ, False]]
+	random.shuffle(shuffleArray)
+	data = {
+		"a" : shuffleArray[0],
+		"b" : shuffleArray[1],
+		"c" : shuffleArray[2],
+	}
+	return data
 
 # get quiz answer & shuffle answers
 def getAnswers(row):
-    try: 
-        colX = sheet.cell(row,3).value
-        colY = sheet.cell(row,4).value
-        colZ = sheet.cell(row,5).value
-        answers = shuffleAnswers(colX, colY, colZ)
-        return answers
-    except:
-        return "Error getAnswers()"
+	try: 
+		colX = sheet.cell(row,3).value
+		colY = sheet.cell(row,4).value
+		colZ = sheet.cell(row,5).value
+		answers = shuffleAnswers(colX, colY, colZ)
+		return answers
+	except:
+		return "Error getAnswers()"
 
 # get question
 def getQuestion(row):
-    try: 
-        question = sheet.cell(row,2).value
-        return question
-    except: 
-        return "Error getQuestion()"
-
-def getInfoURL(row):
-    try: 
-        infoURL = sheet.cell(row,6).value
-        return infoURL
-    except: 
-        return "Error getInfoURL()"
+	try: 
+		question = sheet.cell(row,2).value
+		return question
+	except: 
+		return "Error getQuestion()"
 
 def getInfoGraphic(row):
-    try: 
-        infoGraphic = sheet.cell(row,7).value
-        return infoGraphic
-    except: 
-        return "Error getInfoGraphic()"
+	try: 
+		infoGraphic = sheet.cell(row,6).value
+		return infoGraphic
+	except: 
+		return "Error getInfoGraphic()"
+
+def getInfoURL(row):
+	try: 
+		infoURL = sheet.cell(row,7).value
+		return infoURL
+	except: 
+		return "Error getInfoURL()"
+
+def getFunFact(row):
+	try:
+		funFact = sheet.cell(row,8).value
+		return funFact
+	except:
+		return "Error getFunFact()"
+
 
 def getQuestionRound(row):
 	try: 
 		#TODO: What if the row is empty? Handle it! ==> field should be null
 		question = getQuestion(row)
 		answers = getAnswers(row) 
-		url = getInfoURL(row)		
+		url = getInfoURL(row)
 		image = getInfoGraphic(row)
-		
+		fact = getFunFact(row)
+
 		singleRound = {
 			"question": question,
 			"answers" : answers,
 			"url" : url,
-			"image": image
+			"image": image,
+			"funFact" : fact
 		}
 
 		return singleRound
@@ -89,25 +99,25 @@ def getQuestionRound(row):
 
 
 # TODO: Get topics of questions and where they can be found 
-    #   input = null
-    #   output = strutured 
-    #   output: [
-    # {
-    #        "Rasa Pipeline": [2,6]       
-    #       }, ...
-    # ]
-    # Topic + content is from row x to row y 
+	#   input = null
+	#   output = strutured 
+	#   output: [
+	# {
+	#        "Rasa Pipeline": [2,6]       
+	#       }, ...
+	# ]
+	# Topic + content is from row x to row y 
 
 # TODO: Get a random row
-    # input = null
-    # outpur = row
+	# input = null
+	# outpur = row
 
 # TODO: Get a random row for a specific topic
-    # input = get which rows are relevant ["from row x", "till row y"]
-    # output = row
+	# input = get which rows are relevant ["from row x", "till row y"]
+	# output = row
 
 
 # TODO - last step
-    # write two function which combine these upper function
-    # case 1: random question => output a structure with question, answers etc.
-    # case 2: specific topic question => outuput a structure with question, answers etc. for this topic
+	# write two function which combine these upper function
+	# case 1: random question => output a structure with question, answers etc.
+	# case 2: specific topic question => outuput a structure with question, answers etc. for this topic
